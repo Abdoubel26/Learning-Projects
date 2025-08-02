21143ca29f55fbed6a60d95eb92b02b0f7a8efb1
Abdoubel26 <xenom442025@outlook.com>
2025-08-02 18:46:30 +0100

    good update

--- image slider/logic.js
+++ image slider/logic.js
@@ -1,34 +1,48 @@
 let slideindex = 0;
-let inter
+let interId = 0;
 const images = document.querySelectorAll('.images');
 
 
 document.addEventListener("DOWContentLoaded",initslider())
 
 function initslider() {   
-    images[slideindex].classList.add('displaying');
-
-}
-
-
-
-
-function showimg(index) {   
-
-    if(index >= images.length){
-        slideindex = 0;
+    if(images.length > 0){
+        images[slideindex].classList.add('displaying');
+        interId = setInterval(slideRight, 5000);
+        console.log(interId);    
     }
 }
 
 
+
+function showimg(index) {   
+    if(index + 1 > images.length){
+        slideindex = 0;
+    }
+    else if(index < 0){
+        slideindex = images.length - 1;
+    }
+
+    images.forEach(slide => {
+        slide.classList.remove('displaying');
+    });
+
+    images[slideindex].classList.add('displaying');
+    }
+
+
+
+
 function slideLeft() {  
-    slideindex++;
+    clearInterval(interId);
+    slideindex--;
     showimg(slideindex);
 }
 
 
 function slideRight() {   
-    
+    slideindex++;
+    showimg(slideindex);
 }
 
 
